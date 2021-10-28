d30e498143958b53b23821c4af00d9a686ed8878
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-29 00:38:29 +0600

    app server is listening local port 5000

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
+require( 'dotenv' ).config();
 const express = require( 'express' );
+const app = express();
+const port = process.env.PORT || 5000;
+const cors = require( 'cors' );
+
+app.use( cors() );
+app.use( express.json() );
+
+app.get( '/', ( req, res ) => {
+    res.send( 'Hello World From My Tourism Web Server' );
+} )
+
+app.listen( port, () => {
+    console.log( `Tourism app server is listening at http://localhost:${ port }` )
+} )
