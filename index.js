644f7b554450b70dcc1e21d9af6e4c231a79f396
Sami Meadad Choudhury <sami.meadad@gmail.com>
2021-10-30 00:00:01 +0600

    mongodb database connected rooms data send to the client

--- index.js
+++ index.js
@@ -3,12 +3,42 @@ const express = require( 'express' );
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require( 'cors' );
+const ObjectId = require( 'mongodb' ).ObjectId;
 
+//middleware
 app.use( cors() );
 app.use( express.json() );
 
+const { MongoClient } = require( 'mongodb' );
+
+const uri = `mongodb+srv://${ process.env.DB_USER }:${ process.env.DB_PASS }@cluster0.iezc6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+
+const client = new MongoClient( uri, { useNewUrlParser: true, useUnifiedTopology: true } );
+
+const run = async () => {
+    try {
+        await client.connect();
+        console.log( 'database connected' );
+
+        const database = client.db( "bayViewHotel" );
+        const roomsCollection = database.collection( "rooms" );
+
+        //GET API
+        app.get( '/rooms', async ( req, res ) => {
+            const cursor = roomsCollection.find( {} );
+            const rooms = await cursor.toArray();
+            res.send( rooms );
+        } );
+    }
+    finally {
+        // await client.close();
+    }
+}
+
+run().catch( console.dir );
+
 app.get( '/', ( req, res ) => {
-    res.send( 'Hello World From My Tourism Web Server' );
+    res.send( 'Running Toursim Local Server' );
 } )
 
 app.listen( port, () => {
